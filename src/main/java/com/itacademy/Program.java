1d5bdcbfe7558ae63557e34d8adbad0bd3184e4f
birkickristijan96-hash <birkickristijan96@gmail.com>
2025-12-26 23:55:38 +0100

    Prvi src code Program.java

---
+++ src/main/java/com/itacademy/Program.java
@@ -0,0 +1,18 @@
+package com.itacademy;
+
+/**
+ * kljucne rijeci u javi
+ * package -- predstavlja deklaraciju foldera u kojem se nalazi java klasa .java fajl
+ *public - modifikator vidljivosti
+ * class - deklaracija klase
+ * {mi se krecemo i pisemo kod u tijelu klase }
+ * Case Sensitive -- osjetljiva na velika i mala slova -- konvencija
+ * psvm - main - mora biti u tijelu klase -- glavna funkcija main od koje sve pocinje, java mora od nekud poceti
+ *
+ *
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("vozdra raja dobrodosli u it academy"); //slanje u konzolu, sistemski output
+    }
+}
