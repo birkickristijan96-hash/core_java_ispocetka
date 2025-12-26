b1a4c00b49597fe8f54c900b935cec759ef825c4
birkickristijan96-hash <birkickristijan96@gmail.com>
2025-12-27 00:01:33 +0100

    dodana poruka u three demo

--- src/main/java/com/itacademy/three/ThreeDemo.java
+++ src/main/java/com/itacademy/three/ThreeDemo.java
@@ -2,6 +2,6 @@ package com.itacademy.three;
 
 public class ThreeDemo {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("hello world");
     }
 }
